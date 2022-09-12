ad1bd4b70a485aa5fd87ca95cb690bdfa5edae86
Marcel <marcel.roux.email@gmail.com>
2022-09-12 10:01:06 +0200

    Incorporated file hashing and comparison between 2 slightly different files.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from lib.file import read_file_chunks
+from lib.file import read_file_chunks, compare_hashes, compare_chunk_hashes
 
 
 FILENAME = 'data/sample_file.txt'
+FILENAME_CHANGED = 'data/sample_file_changed.txt'
 
 
 def main():
     print('Hello, World!')
 
-    read_file_chunks(FILENAME)
+    h1, h1_chunks = read_file_chunks(FILENAME)
+    h2, h2_chunks = read_file_chunks(FILENAME_CHANGED)
+
+    print(f'h1: {h1}')
+    print(f'h2: {h2}')
+    print(f'compare_hashes: {compare_hashes(h1=h1, h2=h2)}')
+
+    print(f'h1_chunks:\n{h1_chunks}')
+    print(f'h2_chunks:\n{h2_chunks}')
+    print(f'compare_chunk_hashes: {compare_chunk_hashes(h1_chunks=h1_chunks, h2_chunks=h2_chunks)}')
 
 
 if __name__ == '__main__':
