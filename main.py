29b7c820c4ca00ab4d9e34e5b7e8e413400536ca
Marcel <marcel.roux.email@gmail.com>
2022-09-12 09:00:49 +0200

    Added sample loop for reading file.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from lib.file import read_file_chunks
+
+
+FILENAME = 'data/sample_file.txt'
+
+
 def main():
     print('Hello, World!')
+
+    read_file_chunks(FILENAME)
 
 
 if __name__ == '__main__':
