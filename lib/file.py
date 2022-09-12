44bab5b7dd93ac34656ba2d13010b78ce666b44d
Marcel <marcel.roux.email@gmail.com>
2022-09-12 09:59:56 +0200

    Added hashing of file and chunks.

--- lib/file.py
+++ lib/file.py
@@ -1,7 +1,27 @@
+from hashlib import sha256
+import sys
+
+
 def read_file_chunks(filename: str, chunk_size: int = 1024):
     '''
     A function to lazy load a file with a specified chunk size.
     '''
-    with open(filename) as file:
+    # Whole file's hash.
+    h = sha256()
+
+    # Individual chunk hashes.
+    h_chunks = []
+    with open(filename, 'rb') as file:
         while chunk := file.read(chunk_size):
-            print(chunk)
+            # print(chunk)
+            h.update(chunk)
+
+            h_chunk = sha256()
+            h_chunk.update(chunk)
+            h_chunks.append(h_chunk.hexdigest())
+
+            # print(f'chunk size: {sys.getsizeof(chunk)}')
+            # print(f'chunk_hash size: {sys.getsizeof(h_chunk)}')
+
+    return h.hexdigest(), h_chunks
+
